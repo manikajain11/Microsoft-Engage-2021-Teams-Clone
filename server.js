9ca15993b99126002eb2f6e652209b1d93f8eb68
Manika Jain <72864182+manikajain11@users.noreply.github.com>
2021-07-12 20:57:39 +0530

    updating server.js

--- server.js
+++ server.js
@@ -3,14 +3,7 @@ const app = express();
 const server = require("http").Server(app);
 const { v4: uuidv4 } = require("uuid");
 const io = require("socket.io")(server);
-//enabling TURN/STUN server
-const configuration = {
-  iceServers: [{   
-    urls: [ "stun:bn-turn1.xirsys.com" ]}, {   
-      username: "cGWtB0oCXpelZRG2qHeb7kjCMDzBh_0ou-vJnlodd4DiQ9Bz297BXKt8iBJ9x5H9AAAAAGDsU5htYW5pa2FqYWluMTE=",   
-      credential: "a52324b0-e31e-11eb-86f2-0242ac140004",   
-      urls: [       "turn:bn-turn1.xirsys.com:80?transport=udp"   ]}]
-}
+
 // Peer
 
 const { ExpressPeerServer } = require("peer");

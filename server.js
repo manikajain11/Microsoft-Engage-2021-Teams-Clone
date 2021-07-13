19dc3c47d1bfe2e15f82e95437dcc2e98b55bf09
Manika Jain <72864182+manikajain11@users.noreply.github.com>
2021-07-13 21:57:59 +0530

    updating server.js

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
+/* --------------------------------------------------- */
+/*         DEPLOYING EXPRESS JS                        */
+/* --------------------------------------------------- */
 const express = require("express");
 const app = express();
 const server = require("http").Server(app);
-const { v4: uuidv4 } = require("uuid");
-const io = require("socket.io")(server);
+const { v4: uuidv4 } = require("uuid");       //for unique room URL and ID
+const io = require("socket.io")(server);      //utilizing web sockets to deploy real time communications
 
-// Peer
 
+/* --------------------------------------------------- */
+/*         PEER SERVER                                 */
+/* --------------------------------------------------- */
 const { ExpressPeerServer } = require("peer");
 const peerServer = ExpressPeerServer(server, {
   debug: true,
@@ -15,6 +20,7 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use("/peerjs", peerServer);
 
+//redirects the user to the required Room_ID
 app.get("/", (req, rsp) => {
   rsp.redirect(`/${uuidv4()}`);
 });
@@ -23,15 +29,22 @@ app.get("/:room", (req, res) => {
   res.render("room", { roomId: req.params.room });
 });
 
+/* --------------------------------------------------- */
+/*         CONNECTION OF PARTICIPANTS                  */
+/* --------------------------------------------------- */
 io.on("connection", (socket) => {
   socket.on("join-room", (roomId, userId) => {
     socket.join(roomId);
     socket.to(roomId).broadcast.emit("user-connected", userId);
 
     socket.on("message", (message) => {
-      io.to(roomId).emit("createMessage", message);
+      io.to(roomId).emit("createMessage", message);         //creates a message that a participant has joined the room
     });
   });
 });
 
+//port services
 server.listen(process.env.PORT || 3030);
+
+
+
